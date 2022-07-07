1c9fd836df055db7cfa259e1290f930457880218
adetolab <102938479+adetolab@users.noreply.github.com>
2022-07-07 12:30:56 +0100

    Dee did this
    It is what it is

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+2348134712132']
+global.premium = ['+2348134712132']
+global.ownernomer = '+2348134712132'
+global.ownername = 'adetolab🐤'
+global.botname = 'adetolab🐤'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
